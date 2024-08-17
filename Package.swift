f728fc6913ab42ad9908b028c23b6a7b35485325
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-17 18:39:53 +0200

    chore: remove SwiftLint dependency because it seems to be not working on Linux

--- Package.swift
+++ Package.swift
@@ -15,22 +15,18 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1")),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", .upToNextMajor(from: "0.56.1")),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.54.0")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1"))
     ],
     targets: [
         .target(
             name: "Voltaserve",
             dependencies: ["Alamofire"],
-            path: "Sources",
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            path: "Sources"
         ),
         .testTarget(
             name: "VoltaserveTests",
             dependencies: ["Voltaserve"],
-            path: "Tests",
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            path: "Tests"
         )
     ]
 )
