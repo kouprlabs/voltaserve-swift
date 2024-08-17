99090a24b10f2217a28e57dfe0f7c7502e26a16a
Anass Bouassaba <ebouassaba@gmail.com>
2024-08-17 08:02:21 +0200

    wip: implement token

--- Sources/Core/Token.swift
+++ Sources/Core/Token.swift
@@ -3,6 +3,7 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
+import Alamofire
 import Foundation
 
 public struct VOToken {
@@ -16,17 +17,71 @@ public struct VOToken {
 
     // MARK: - Requests
 
+    public func exchange(options: ExchangeOptions) async throws -> Value {
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(
+                url(),
+                method: .post,
+                parameters: options.urlParameters,
+                encoding: URLEncoding.default,
+                headers: headersWithAuthorization(accessToken)
+            ).responseData { response in
+                handleJSONResponse(continuation: continuation, response: response, type: Value.self)
+            }
+        }
+    }
+
     // MARK: - URLs
+
+    public func url() -> URL {
+        URL(string: "\(baseURL)/v2/token")!
+    }
 
     // MARK: - Payloads
 
+    public struct ExchangeOptions {
+        public let grantType: GrantType
+        public let username: String?
+        public let password: String?
+        public let refreshToken: String?
+        public let locale: String?
+
+        var urlParameters: [String: String] {
+            var params: [String: String] = ["grant_type": grantType.rawValue]
+            if let username {
+                params["username"] = username
+            }
+            if let password {
+                params["password"] = password
+            }
+            if let refreshToken {
+                params["refresh_token"] = refreshToken
+            }
+            if let locale {
+                params["locale"] = locale
+            }
+            return params
+        }
+    }
+
+    public enum GrantType: String {
+        case password
+        case refreshToken = "refresh_token"
+    }
+
     // MARK: - Types
 
-    public struct Value {
+    public struct Value: Codable {
         public var accessToken: String
+        public var expiresIn: Int
+        public var tokenType: String
+        public var refreshToken: String
 
-        public init(accessToken: String) {
-            self.accessToken = accessToken
+        enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+            case expiresIn = "expires_in"
+            case tokenType = "token_type"
+            case refreshToken = "refresh_token"
         }
     }
 }
