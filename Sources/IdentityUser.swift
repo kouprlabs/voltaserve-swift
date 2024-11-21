008d2997fe63c25fd1475697c4da8a0f6291c66d
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-21 04:56:13 +0100

    chore: update license header

--- Sources/IdentityUser.swift
+++ Sources/IdentityUser.swift
@@ -1,3 +1,10 @@
+// Copyright 2024 Anass Bouassaba.
+//
+// This software is licensed under the MIT License.
+// You can find a copy of the license in the LICENSE file
+// included in the root of this repository or at
+// https://opensource.org/licenses/MIT.
+
 import Foundation
 
 public struct VOIdentityUser {
