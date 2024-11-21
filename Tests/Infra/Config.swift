5084291c16cee58c9569320f6d72f7a5c608df5b
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-21 06:57:56 +0100

    chore: refine license header (#10)

--- Tests/Infra/Config.swift
+++ Tests/Infra/Config.swift
@@ -1,9 +1,8 @@
 // Copyright (c) 2024 Anass Bouassaba.
 //
 // This software is licensed under the MIT License.
-// You can find a copy of the license in the LICENSE file
-// included in the root of this repository or at
-// https://opensource.org/licenses/MIT.
+// See the LICENSE file in the root of this repository for details,
+// or visit <https://opensource.org/licenses/MIT>.
 
 import Foundation
 import VoltaserveCore
